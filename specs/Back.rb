d515ec6eb25a5d15f28e759adaa2930aa0f92e3c
da99 <i-hate-spam-1234567@mailinator.com>
2014-04-04 09:07:42 -0500

    Added: specs for .top?, .bottom?, .middle?

--- specs/Back.rb
+++ specs/Back.rb
@@ -91,6 +91,36 @@ describe "Back" do
 
     end # === describe next ===
 
+    describe ".top?" do
+
+      it "returns true if at real first" do
+        track = []
+        About_Pos.Back([1,2,3,4,5]) { |v,i,m| track.push m.top?  }
+        track.should == [false, false, false, false, true]
+      end
+
+    end # === describe .top? ===
+
+    describe ".middle?" do
+
+      it "returns true if not at real first and real last" do
+        track = []
+        About_Pos.Back([4,5,6]) { |v,i,m| track.push m.middle?  }
+        track.should == [false, true, false]
+      end
+
+    end # === describe .middle? ===
+
+    describe ".bottom?" do
+
+      it "returns true if at real last" do
+        track = []
+        About_Pos.Back([10,11,12,15]) { |v,i,m| track.push m.bottom?  }
+        track.should == [true, false, false, false]
+      end
+
+    end # === describe .bottom? ===
+
   end # === describe Meta ===
 
 end # === describe about_pos ===
