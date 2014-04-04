79fc4c18fab8a790f81e92e9888332bd0ee9ea81
da99 <i-hate-spam-1234567@mailinator.com>
2014-04-03 20:58:48 -0500

    More specs for Back. More docs

--- README.md
+++ README.md
@@ -10,3 +10,9 @@ Loop through an array and keep track of previous, next item, current item, etc
 ## Usage
 
 No coming any time soon.
+
+## The Essence of the Implementation
+
+  * `meta.next`, `meta.prev`.
+  * saving data (`[]`, `[]=`) and keep sure
+    the data is carried over to the other items.
--- specs/Back.rb
+++ specs/Back.rb
@@ -17,4 +17,31 @@ describe "Back" do
     track.should == [2, 1, 0]
   end
 
+  describe "Meta" do
+
+    describe "prev?" do
+
+      it "is false when at first item (real last item)" do
+        track = []
+        About_Pos.Back([1,2,3]) do |v, i, m|
+          track.push m.prev?
+        end
+        track.should == [false, true, true]
+      end
+
+    end # === describe prev? ===
+
+    describe "next?" do
+
+      it "is first if it reaches the last item (real first item)" do
+        track = []
+        About_Pos.Back([4,5,6]) do |v,i,m|
+          track.push m.next?
+        end
+        track.should == [true, true, false]
+      end
+    end # === describe next? ===
+
+  end # === describe Meta ===
+
 end # === describe about_pos ===
